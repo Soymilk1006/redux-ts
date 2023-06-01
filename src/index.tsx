6031856bee4a88d146053c63c800a8e35ae2f4e9
Deven Li <deven_li@gmail.com>
2023-06-01 10:37:36 +1000

    adjust and rebase change

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,8 @@ root.render(
 // new create branch and fix another bug
 // If you want to start measuring performance in your app, pass a function
 //test a new Line
-////add a new line and good
+//add a new line and good
+//add a second line
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
